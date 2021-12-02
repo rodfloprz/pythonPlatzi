5c4e6cd6e52ae3c9fdc64275a3c17e0463828915
Rodrigo Flores <ing.rodrigo.flores@gmail.com>
2021-12-01 21:42:50 -0600

    First commint in python

---
+++ 1_variables.py
@@ -0,0 +1,24 @@
+print("Mi primer programa en python")
+
+numero1=5
+numero2=6
+
+total= numero1+numero2
+
+print("I will show information regarding variables")
+
+print("El resultado de la suma var1: ")
+print(numero1) 
+print("mas var2: ") 
+print(numero2)
+print("es: ")
+print(total)
+
+numero_decimal=3.4
+nombre="Rodrigo"
+apellido='Flores'
+is_student=True
+
+print(is_student)
+print(nombre*4)
+print(nombre+" "+apellido)
